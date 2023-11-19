fb760a7f8b22db73f9cb77e6f44cf55f8ba03bfa
Moafk <mwafak_sh@hotmail.com>
2023-11-19 12:46:51 +0300

    Wrote login method

--- src/app/auth/auth.services.ts
+++ src/app/auth/auth.services.ts
@@ -3,14 +3,14 @@ import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
 
-interface AuthResponseData {
+export interface AuthResponseData {
     kind: string,
     idToken: string,
     email:string,
     refreshToken: string,
     expiresIn: string,
-    
     localId: string
+    registered?: boolean
 }
 
 @Injectable({providedIn:'root'})
@@ -34,5 +34,16 @@ export class AuthServices {
                 }
                 return throwError(errorMessage);
             }))
+    }
+
+    login(email: string, password:string){
+        return this.http.post<AuthResponseData>(
+            'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCILqxCAyaKWmaNI9icpNMTXTZiCMGLFSw'
+            ,{
+                email: email,
+                password: password,
+                returnSecureToken : true
+            }
+            );
     }
 }
